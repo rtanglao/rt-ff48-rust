bc485ae389745c6fa38b6e294cbd7d4698fbbb3d
Roland Tanglao <roland@rolandtanglao.com>
2016-08-17 22:46:05 -0700

    commits_since()

--- get-rust-mp4parse-commits.rb
+++ get-rust-mp4parse-commits.rb
@@ -8,7 +8,7 @@ client = Octokit::Client.new(:netrc => true)
 #repo = Octokit.repo 'mozilla/mp4parse-rust'
 #pp repo
 printf("authorid,login,commitdate\n")
-pp  Octokit.commits('mozilla/mp4parse-rust')[0]
-Octokit.commits('mozilla/mp4parse-rust').each do |commit|
+pp  Octokit.commits_since('mozilla/mp4parse-rust', '2016-01-01')[0]
+Octokit.commits_since('mozilla/mp4parse-rust', '2016-01-01').each do |commit|
   printf("%d,%s,%s\n", commit.author.id, commit.author.login, commit.commit.author.date)
 end
