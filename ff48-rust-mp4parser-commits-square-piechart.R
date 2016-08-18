382bc32dc9261fdba7829c8bdd2d41637b955182
Roland Tanglao <roland@rolandtanglao.com>
2016-08-18 15:41:30 -0700

    square pie chart with 400 commits

--- ff48-rust-mp4parser-commits-square-piechart.R
+++ ff48-rust-mp4parser-commits-square-piechart.R
@@ -1,16 +1,19 @@
 library(ggplot2)
 library(waffle)
+library(plyr)
 
 actualrustmp4commits = read.csv(
-  file="actualrustmp4commits.csv", 
+  file="mp4parse-commits-without-commit-reason.csv", 
     stringsAsFactors=F)
 
-colour_vector <-setNames(actualrustmp4commits$numcommits,
-actualrustmp4commits$person)
-colour_vector
+countname = count(actualrustmp4commits, "name")
+countname <- countname[order(-countname$freq),]
 
-p = waffle(colour_vector, rows=5, size=0.5) + theme_void() +
-theme(legend.position="none")
+waffle_vector <-setNames(countname$freq, countname$name)
+waffle_vector
 
-ggsave("ff48rustmp4parser-commit-square-piechart.png", p, width = 26.666666667, height = 26.666666667, dpi = 72, limitsize = FALSE) # 26.6666667 = 1920/72dpi
+p = waffle(waffle_vector, rows=5, size=0.5) + theme_void() +
+theme(legend.position="none") #+ scale_color_brewer(palette="Paired")
+
+ggsave("400commits-ff48rustmp4parser-commit-square-piechart.png", p, width = 26.666666667, height = 26.666666667, dpi = 72, limitsize = FALSE) # 26.6666667 = 1920/72dpi
 warnings()
