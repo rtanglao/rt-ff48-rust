9bee5a6221717257a7b41dc98cb7de79df7650fc
Roland Tanglao <roland@rolandtanglao.com>
2016-08-17 22:24:32 -0700

    print out id, login, date

--- get-rust-mp4parse-commits.rb
+++ get-rust-mp4parse-commits.rb
@@ -4,9 +4,11 @@ require 'json'
 require 'pp'
 require 'octokit'
 client = Octokit::Client.new(:netrc => true)
-pp client.login
 # get commits from https://github.com/mozilla/mp4parse-rust
 #repo = Octokit.repo 'mozilla/mp4parse-rust'
 #pp repo
-commits = Octokit.commits('mozilla/mp4parse-rust')
-pp commits
+printf("authorid,login,commitdate\n")
+pp  Octokit.commits('mozilla/mp4parse-rust')[0]
+Octokit.commits('mozilla/mp4parse-rust').each do |commit|
+  printf("%d,%s,%s\n", commit.author.id, commit.author.login, commit.commit.author.date)
+end
