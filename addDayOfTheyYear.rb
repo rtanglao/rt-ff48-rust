4b37007ed134a32c5cf4e9a4965bb69da83ac343
Roland Tanglao <roland@rolandtanglao.com>
2016-09-17 08:19:42 -0700

    add 365 if the commit was done in 2016

--- addDayOfTheyYear.rb
+++ addDayOfTheyYear.rb
@@ -12,8 +12,21 @@ CSV.foreach(filename) do |row|
     next
   end
   t = Time.at(row[1].to_i)
-  s = t.strftime("%-j")
+  s = t.yday
+  year = t.year
+  $stderr.printf("year:%d\n", year)
+  if year == 2015
+    s += 365
+    $stderr.printf("year is 2015 ADDING 365: %s\n", s)
+  elsif year == 2014
+    next
+    $stderr.printf("year is 2014 NOT ADDING\n")
+  elsif
+    $stderr.printf("year is NOT 2015 and NOT 2014:%s\n", year)
+  end
+                    
+
   # qq s
-  printf("%s,%s,%s\n",s,row[1],row[0])
+  printf("%d,%s,%s\n",s,row[1],row[0])
 end
  
